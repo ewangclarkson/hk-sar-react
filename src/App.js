bb05ad6133e9073ce414427c708dc11c5de5dbb5
ewangclarkson <ewangclarks@gmail.com>
2023-12-04 02:20:18 +0100

    fix bug

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+import AddPost from "./features/posts/AddPost";
+
+function App() {
+    return (
+        <main className="App">
+            <AddPost/>
+        </main>
+    );
+}
+
+export default App;
